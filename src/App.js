7531278681226fd419067c7ee088d83a4d374e9b
Veronica <veronicahester20@gmail.com>
2022-07-07 15:44:04 +0800

    Added #76

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar';
+import NoScreen from './screens/NoScreen'
 import FooterBar from './components/FooterBar';
 
 class App extends React.Component
@@ -16,6 +17,7 @@ class App extends React.Component
             <div>
               <NavBar/>
                 <h1>Hello React!</h1>
+                <NoScreen/>
                 <FooterBar/>
             </div>
         );
