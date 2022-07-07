4fec6e8ddb1f57e08df1ef20597701c057fb957c
Veronica <veronicahester20@gmail.com>
2022-07-07 16:08:51 +0800

    Added #9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 
+
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+
 import NavBar from './components/NavBar';
+import HomeScreen from './screens/HomeScreen';
+import ProductsScreen from './screens/ProductsScreen';
 import NoScreen from './screens/NoScreen'
 import FooterBar from './components/FooterBar';
-
+import SingleProductScreen from './screens/SingleProductScreen';
 class App extends React.Component
 {
     constructor() {
@@ -16,8 +27,14 @@ class App extends React.Component
         return (
             <div>
               <NavBar/>
-              <NoScreen/>
-                <h1>Hello React!</h1>
+              <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<SingleProductScreen/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
                 <FooterBar/>
             </div>
         );
